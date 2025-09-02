e1736229aee9ef5bdf34ee4f751b73d6a3b9dd15
Kevin Ye <ranchonpizza24@gmail.com>
2025-09-01 22:43:31 -0700

    chore: misc

---
+++ hw1/main.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import SoupStrainer, BeautifulSoup
+import pandas as pd
+import io
+import re
+
+## GET HTML
+
+url = 'https://www.unr.edu/admissions/records/academic-calendar/finals-schedule'
+response = requests.get(url=url)
+
+
+soup = BeautifulSoup(response.text, features='lxml')
+
+tables = soup.find_all('table')
+dfs = []
+for table in tables:
+    df = pd.read_html(io.StringIO(str(table)))[0]
+
+    caption = table.find('caption')
+    caption = caption.text.strip() if caption else None
+    df['Finals Day'] = caption
+
+    dfs.append(df)
+
+df = pd.concat(dfs)
+df.head()
+
+
+# Standardize Class Day(s)
+
+def extract_codes(str):
+    return re.findall(r'\(([A-Z]+)\)', str)
+
+df['Class Day(s)'] = df['Class Day(s)'].apply(extract_codes)
+df = df.explode('Class Day(s)').reset_index(drop=True)
+df.head()
+
+
+# Export generic_finals_schedule.csv
+
+# df.to_csv('../data/generic_finals_schedule.csv')
+
+# Import input.csv
+
+try:
+    student_df = pd.read_csv('data/input.csv')
+    
+except FileNotFoundError as e:
+    print(e)
+    print('Using sample data instead')
+    student_df = pd.read_csv('data/sample_input.csv')
+
+student_df.head()
+
+# Calculate finals schedule (output)
+
+final_schedule_df = student_df.merge(right=df)
+final_schedule_df.drop(['Class Time','Class Day(s)'], axis='columns', inplace=True)
+final_schedule_df
+
+# Export output.csv
+
+final_schedule_df.to_csv('data/output.csv')
+
+
+
