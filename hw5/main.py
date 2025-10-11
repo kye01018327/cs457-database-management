891586449a0177e1cd97a1da4831324e1fc78ce8
Kevin Ye <ranchonpizza24@gmail.com>
2025-10-11 02:37:22 -0700

    feat: finish main

--- hw5/main.py
+++ hw5/main.py
@@ -1,14 +1,20 @@
 import psycopg
+from utils import Interface, Database
 
-# Connect to database
-with psycopg.connect(
-    'dbname=postgres user=postgres password=jdfk host=localhost port=5433'
-) as conn:
-    with conn.cursor() as cur:
-        cur.execute('''
-            SELECT * FROM product
-        ''')
-        result = cur.fetchall()
+db = Database('postgres','postgres','jdfk','localhost',5433)
+app = Interface(db)
+app.start()
 
-for row in result:
-    print(row)
+
+# User interface
+    # Get user input on speed
+        # Query what PC models have a speed of at least that speed (query)
+        # Show table
+    # Get user input on manufacturer
+        # Query model number and type of all products made by that manufacturer (query)
+        # Show table
+        # Give option to select model from list of products
+        # Show table/price
+
+    # Quit
+# psycopg (psycopg3) or psycopg2: same parameter style (%s)
